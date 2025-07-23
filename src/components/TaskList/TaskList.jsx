cce944167b0c1c953a70a3aa555b0ebba0e03761
Pulkit <P00997887@asianpaints.com>
2025-07-23 09:15:10 +0530

    Formatting and other minor fixes

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,16 +1,16 @@
 import '../../styles/components/_task-item.scss';
 import TaskItem from '../TaskItem/TaskItem';
- 
+
 export default function TaskList({ tasks, onEdit, onDelete, onStateChange }) {
   if (tasks.length === 0) {
     return <div className="task-list__empty">No tasks yet</div>;
   }
- 
+
   return (
     <div className="task-list">
-{tasks.map(task => (
+      {tasks.map(task => (
         <TaskItem
-key={task.id}
+          key={task.id}
           task={task}
           onEdit={onEdit}
           onDelete={onDelete}
